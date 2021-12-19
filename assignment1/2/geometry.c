b08b1c20affd35f59a052e3f844daef88553442e
takrim1999 <takrimulalom@gmail.com>
2021-12-19 23:41:14 +0600

    releasing geometry version 2

--- assignment1/2/geometry.c
+++ assignment1/2/geometry.c
@@ -4,61 +4,72 @@ int square();
 int triangle();
 int rectangle();
 int main(){
-    char shape[10];
-    printf("which shape you want to calculate?(square/rectangle/triangle/circle\n)>");
-    scanf("%s",shape);
-    // printf("%s",shape);
-    if (shape == "circle"){
-        printf("done");
-        circle();
-    }
-    else if(shape == "square"){
-        square();
-    }
-    else if(shape == "triangle"){
-        triangle();
-    }
-    else if (shape == "rectangle"){
-        rectangle();
-    }
+    int side;
+    printf("how mane sides your shape contains?\n0=circle\n1=line\n2=angle\n3=triangle\n4=quadrilateral\n>");
+    scanf("%d",&side);
+    switch (side){
 
-    // switch (shape){
-    //     case 'circle':
-    //         printf("hello you");
-        // circle();
-        // break;
-        // case 'square':
-        // square();
-        // break;
-        // case 'triangle':
-        // triangle();
-        // break;
-        // case 'rectangle':
-        // rectangle();
-        // break;
-    // }
-
+        case 0:
+            circle();
+            break;
+        case 1:
+            printf("it is a line,not a covered area\n");
+            break;
+        case 2:
+            printf("it is an angle,not a covered area\n");
+            break;
+        case 3:
+            triangle();
+            break;
+        case 4:
+            square();
+            break;
+        default:
+            printf("our service is still limited, try (0-4) please\n");
+    }
 return 0;
 }
 
 
 
 int circle(){
-float radious,area,volume,perimeter;
-printf("circle function");
-
+float radious=0;
+int dimension=0;
+printf("dimension?(2 or 3)\n>");
+scanf("%d",&dimension);
+if (dimension==2){
+    printf("radious of circle\n>");
+    scanf("%f",&radious);
+    printf("Perimete(circumfarence) : %.3f\n", 2*3.1416*radious);
+    printf("Area : %.3f\n", 3.1416*radious*radious);
+}
+else if (dimension==3){
+    printf("radious of sphere\n>");
+    scanf("%f",&radious);
+    printf("Surface Area : %.3f\n", (4*3.1416*radious*radious));
+    printf("Volume : %.3f\n", (4*3.1416*radious*radious*radious)/3);
+}
+else
+    printf("our geometric calculator only support till 3 dimension\n");
 return 0;
 }
-int square(){
 
-printf("square function");
+int square(){
+int dimension;
+printf("dimension?(2 or 3)\n>");
+scanf("%d",&dimension);
+if (dimension == 2){
+    printf("how many sides are equal?\n4=square\n2=rectangle")
+}
 return 0;
 }
 int triangle(){
-printf("triangle function");
+int dimension;
+printf("dimension?(2 or 3)\n>");
 return 0;
 }
 int rectangle(){
-printf("rectangle function");
+int dimension;
+printf("dimension?(2 or 3)\n>");
 return 0;
 }
