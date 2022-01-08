a3341cd17fa90103d67a405225986851f23fe836
takrim1999 <takrimulalom@gmail.com>
2022-01-08 23:35:23 +0600

    written 4 no template

---
+++ assignment4/4/prime.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int prime(int start, int end);
+int main(){
+    int start=0,end=0;
+    printf("input starting point\n>");
+    scanf("%d",&start);
+    printf("input ending point\n>");
+    scanf("%d",&end);
+    prime(start,end);
+    return 0;
+}
+
+int prime(int s, int e){
+    int plist[e-s+1];
+    for(int i = 0; i<=(e-s+1); i++){
+        plist[i] = i;
+    }
+    for(int i = 0; i<(e-s); i++){
+        printf("%d\n",plist[i]);
+    }
+    return 0;
+}
