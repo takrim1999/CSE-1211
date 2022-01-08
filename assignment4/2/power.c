9cccf4ba8dad3a3ff2849063151e21d81c886e7e
takrim1999 <takrimulalom@gmail.com>
2022-01-08 21:18:04 +0600

    written template to 4

---
+++ assignment4/2/power.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int power(float x, float y);
+int main(){
+    float y,x;
+    printf("the number you want to calculate power of\n>");
+    scanf("%f",&y);
+    printf("the number you want to calculate to the power\n>");
+    scanf("%f",&x);
+    printf("%.2f to the power %d is %.2f",x,power(x,y),y);
+    return 0;
+}
+
+int power(float x, float y){
+    int n = 0;
+    
+    return n;
+}
