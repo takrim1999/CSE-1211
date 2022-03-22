58aeef662abd4e48fc911fb64e0e13d0fa18e14c
takrim1999 <takrimulalom@gmail.com>
2022-03-22 11:28:42 +0600

    Questions

---
+++ test/2.c
@@ -0,0 +1,15 @@
+/*
+This is the second problem from lab test.
+Problem : Count frequencies of vowel,consonents,whitespaces from a string.
+Sample input: Hey Brother
+Sample Output : 
+Vowels: 3
+consonents : 7
+whitespaces : 1
+*/
+#include<stdio.h>
+int main(){
+    
+
+
+}
